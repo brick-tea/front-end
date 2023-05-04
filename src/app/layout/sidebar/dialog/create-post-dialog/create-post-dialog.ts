fb33e3462a4d8a08dd7cbc69cdd969944f7a3272
wk00100 <wanderwalker0110@protonmail.com>
2023-05-04 10:00:06 +0800

    update post

--- src/app/layout/sidebar/dialog/create-post-dialog/create-post-dialog.ts
+++ src/app/layout/sidebar/dialog/create-post-dialog/create-post-dialog.ts
@@ -29,6 +29,10 @@ export class CreatePostDialog implements OnInit {
     productsId: [],
   };
   ngOnInit(): void {
+    if (this.data) {
+      this.isEdit = true;
+      this.post = this.data.post;
+    }
     this.product.getMyProducts().subscribe(
       (products) => {
         this.proudctList = [];
@@ -38,9 +42,7 @@ export class CreatePostDialog implements OnInit {
             title: name,
           } as ProductEssansial);
         }
-        if (this.data) {
-          this.isEdit = true;
-          this.post = this.data.post;
+        if (this.isEdit) {
           for (let i = 0; i < this.post.productsId.length; i++) {
             for (let j = 0; j < this.proudctList.length; j++) {
               if (this.post.productsId[i] === this.proudctList[j].productId) {
@@ -60,8 +62,10 @@ export class CreatePostDialog implements OnInit {
   debug() {
     console.log(this.isSelectProduct);
   }
-
+  isSending: boolean = false;
   onSubmit() {
+    this.isSending = true;
+    this.post.productsId = [];
     console.log(this.isSelectProduct);
     for (let i = 0; i < this.proudctList.length; i++) {
       if (this.isSelectProduct[i]) {
@@ -69,23 +73,37 @@ export class CreatePostDialog implements OnInit {
       }
     }
     console.log(this.post);
-    this.postsService.addPost(this.post).subscribe(
-      (res) => {
-        console.log(res);
-        alert('成功發布！');
-        this.dialogRef.close();
-      },
-      (err) => {
-        console.log(err);
-        alert('發布失敗，請重試！');
-        if (err.status === 403) {
+    if (this.isEdit) {
+      this.postsService.editPost(this.data.postId, this.post).subscribe(
+        () => {
+          alert('成功更新！');
+          this.dialogRef.close(true);
+        },
+        (err) => {
+          console.log(err);
+          this.isSending = false;
         }
-      }
-    );
+      );
+    } else {
+      this.postsService.addPost(this.post).subscribe(
+        (res) => {
+          console.log(res);
+          alert('成功發布！');
+          this.dialogRef.close(true);
+        },
+        (err) => {
+          console.log(err);
+          this.isSending = false;
+          alert('發布失敗，請重試！');
+          if (err.status === 403) {
+          }
+        }
+      );
+    }
   }
 
   onNoClick() {
-    this.dialogRef.close();
+    this.dialogRef.close(false);
   }
 }
 interface ProductEssansial {
